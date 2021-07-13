c513e8dfdea6d39f011e8eca7d5c414ea6d79448
aciulei <Andrada.Ciulei@centric.eu>
2021-07-13 22:13:30 +0300

    Updated

--- trip-looking/src/app/models/trip.ts
+++ trip-looking/src/app/models/trip.ts
@@ -2,6 +2,11 @@ export class Trip {
   id?: string;
   title: string;
   description: string;
-  photos: any;
   private: boolean;
+
+  public constructor(title: string, description: string, privateFlag: boolean) {
+    this.title = title;
+    this.description = description;
+    this.private = privateFlag;
+  }
 }
